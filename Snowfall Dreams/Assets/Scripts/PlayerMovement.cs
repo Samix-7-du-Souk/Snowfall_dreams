e88a57c0df670fab034a811a191def3c0f35b778
Bismuth <bismuthdev@gmail.com>
2021-07-23 19:18:05 +0200

    Add GroundCheck

--- Snowfall Dreams/Assets/Scripts/PlayerMovement.cs
+++ Snowfall Dreams/Assets/Scripts/PlayerMovement.cs
@@ -5,17 +5,22 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed;
     public float jumpForce;
 
-    public bool isJumping = false;
-    
+    public bool isJumping;
+    public bool isGrounded;
+
+    public Transform groundCheckLeft;
+    public Transform groundCheckRight;
 
     public Rigidbody2D rb;
     private Vector3 velocity = Vector3.zero;
 
     void FixedUpdate()
     {
+        isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
+        
         float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             isJumping = true;
         }
