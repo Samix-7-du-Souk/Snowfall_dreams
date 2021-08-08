ab3d3fe2f43a2811ab1436fd0834b79f2c0f4669
Samix-7 <77936812+Samix-7@users.noreply.github.com>
2021-08-08 23:07:41 +0200

    Rajout des pv (incomplet)
    conteneur et pv

---
+++ Snowfall Dreams/Assets/Scripts/Pv.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pv : MonoBehaviour
+{
+    public int pv;
+    public int numPv;
+
+    public Image[] couers;
+    public Sprite fullvie;
+    public Sprite zerovie;
+
+   
+}
+ 
+
