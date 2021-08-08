faf875258299fade4dd71e0eae1d3296a5790179
Samix-7 <77936812+Samix-7@users.noreply.github.com>
2021-08-09 01:15:36 +0200

    pv fini
    systeme de pv

--- Snowfall Dreams/Assets/Scripts/Pv.cs
+++ Snowfall Dreams/Assets/Scripts/Pv.cs
@@ -5,14 +5,41 @@ using UnityEngine.UI;
 
 public class Pv : MonoBehaviour
 {
-    public int pv;
-    public int numPv;
+    public int pv;      // les point de viesau joeurs    //
+    public int numPv; // le nbr de pv max //
 
     public Image[] couers;
     public Sprite fullvie;
     public Sprite zerovie;
 
-   
+    void Update()
+    {
+        if (pv > numPv)
+        {
+            pv = numPv;
+        }
+        for (int i = 0; i < couers.Length; i++) // i = nbr de couers afficher//
+        {
+            if (i < pv)
+            {
+                couers[i].sprite = fullvie;
+            }
+            else
+            {
+                couers[i].sprite = zerovie;
+            }
+            if (i < numPv)
+            {
+                couers[i].enabled = true;
+            }
+            else
+            {
+                couers[i].enabled = false;
+            }
+        }
+    }
+
+
 }
- 
+
 
