14a646d5def30d5aca9eef6136e8e249edae2eb3
10K_Bismuth <bismuthdev@gmail.com>
2021-08-12 20:03:55 +0200

    Add wall check and start implement wall thingy (wall grab for now)

--- Snowfall Dreams/Assets/Scripts/PlayerController.cs
+++ Snowfall Dreams/Assets/Scripts/PlayerController.cs
@@ -6,11 +6,21 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
     
     public Rigidbody2D theRB;
-    
+
     private bool isGrounded;
     public Transform groundCheck;
     public float groundCheckRadius;
     public LayerMask collisionLayers;
+    // For walls thingy
+    private bool onWall;
+    private bool onRightWall;
+    private bool onLeftWall;
+    public bool wallGrab;
+    public int side;
+    public Transform groundCheckLeft;
+    public Transform groundCheckRight;
+    public float groundCheckRadiusSide;
+    public LayerMask climbableWalls;
     
     public float hangTime = .2f;
     private float hangCounter;
@@ -29,6 +39,32 @@ public class PlayerController : MonoBehaviour
         
         //Check if is on ground thank to an OverlapCircle
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, collisionLayers);
+        
+        onLeftWall = Physics2D.OverlapCircle(groundCheckLeft.position, groundCheckRadiusSide, climbableWalls);
+        onRightWall = Physics2D.OverlapCircle(groundCheckRight.position, groundCheckRadiusSide, climbableWalls);
+        onWall = onLeftWall || onRightWall;
+        // If player is on right wall output 1 else -1 -> ternary conditional operator
+        side = onRightWall ? 1 : -1;
+
+        if (onWall && Input.GetButton("Grab"))
+        {
+            wallGrab = true;
+        }
+        
+        if (!onWall || Input.GetButtonUp("Grab"))
+        {
+            wallGrab = false;
+        }
+
+        if (wallGrab)
+        {
+            theRB.gravityScale = 0;
+            theRB.velocity = new Vector2(theRB.velocity.x, 0);
+        }
+        else
+        {
+            theRB.gravityScale = 1;
+        }
         
         // Manage hangtime
         if (isGrounded) {
@@ -66,5 +102,7 @@ public class PlayerController : MonoBehaviour
         // Draw an gizmo to represent the OverlapCircle
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        Gizmos.DrawWireSphere(groundCheckLeft.position, groundCheckRadiusSide);
+        Gizmos.DrawWireSphere(groundCheckRight.position, groundCheckRadiusSide);
     }
 }
