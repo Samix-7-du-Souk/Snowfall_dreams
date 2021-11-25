26c3037e5a77404c3f3e02598de95cb7fc8d9c91
Samix-7 <77936812+Samix-7@users.noreply.github.com>
2021-11-25 16:08:17 +0100

    weapons wheel setup

--- Snowfall Dreams/Assets/Scripts/PlayerCombatSystem.cs
+++ Snowfall Dreams/Assets/Scripts/PlayerCombatSystem.cs
@@ -4,15 +4,71 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Animator animator;
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+    public int selectWeapon = 0;
 
-    // Update is called once per frame
+    private void Start()
+    {
+        SelectWeapon();
+    }
     void Update()
     {
-        
+        if (Input.GetButtonDown("SpearAttack"))
+        {
+            Attack();
+        }
+    }
+
+    void SelectWeapon()
+    {
+        int w = 0;
+        foreach (Transform weapon in transform)
+        {
+            if (w == selectWeapon)
+            {
+                weapon.gameObject.SetActive(true);
+
+            }
+        }
+    }
+
+    void Attack(int dealDamage)
+    {
+        if ((Input.GetButtonDown("Attack"))
+        {
+            if (selectWeapon = 0)
+            {
+                animtor.SetTrigger("AttackSword");
+                Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+                dealDamage =
+            }
+            if (selectWeapon = 1)
+            {
+                animtor.SetTrigger("AttackArrow");
+                Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+            }
+            if (selectWeapon = 2)
+            {
+                animtor.SetTrigger("AttackSpear");
+                Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+            }
+            if (selectWeapon = 3)
+            {
+                animtor.SetTrigger("AttackHammer");
+                Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+            }
+
+        }
+
+
+        foreach (Collider2D in hitEnemies)
+        {
+            Debug.Log("We hit" + enemy.name);
+
+        }
     }
 }
+
